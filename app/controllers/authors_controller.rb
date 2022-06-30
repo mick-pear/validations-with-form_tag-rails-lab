08c26c24a484d83b3f4022bcbfa7d66a1bc5b0df
Matthew <matthew_mcaleer@hotmail.com>
2022-06-30 11:45:51 +1000

    commit tree 410f8d4488a0864f4f2fb0aa56e6f2c54af2b32a

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -13,6 +13,21 @@ class AuthorsController < ApplicationController
     redirect_to author_path(@author)
   end
 
+    def edit
+        @author = Author.find(params[:id])
+    end
+
+    def update
+        @author = Author.find(params[:id])
+
+        if @author.valid?
+            @author.update(author_params)
+            redirect_to author_path(@author)
+        else
+            render :edit
+        end
+    end
+
   private
 
   def author_params
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,30 @@
 class PostsController < ApplicationController
-  def show
-    @post = Post.find(params[:id])
-  end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+    def new
+    end
+  
+    def show
+        @post = Post.find(params[:id])
+    end
 
-  def update
-    @post = Post.find(params[:id])
+    def edit
+        @post = Post.find(params[:id])
+    end
 
-    @post.update(post_params)
+    def update
+        @post = Post.find(params[:id])
 
-    redirect_to post_path(@post)
-  end
+        if @post.valid?
+            @post.update(post_params)
+            redirect_to post_path(@post)
+        else
+            render :edit
+        end
+    end
 
-  private
+    private
 
-  def post_params
-    params.permit(:title, :category, :content)
-  end
+        def post_params
+            params.permit(:title, :category, :content)
+        end
 end
